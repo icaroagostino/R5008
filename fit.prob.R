7662719dca9d830867bc967502e79a208c30e828
Ícaro Agostino <icaroagostino@gmail.com>
2020-07-06 05:49:02 +0200

    Create fit.prob.R

---
+++ fit.prob.R
@@ -0,0 +1,34 @@
+library(fitdistrplus)
+
+fit.prob <- function(y){
+  
+  y <- as.numeric(y)
+  
+  norm    <- fitdistrplus::fitdist(y, distr = "norm")
+  gamma   <- fitdistrplus::fitdist(y, distr = "gamma")
+  weibull <- fitdistrplus::fitdist(y, distr = "weibull")
+  lnorm   <- fitdistrplus::fitdist(y, distr = "lnorm")
+  exp     <- fitdistrplus::fitdist(y, distr = "exp")
+  
+  all_dist <- list(norm,
+                   gamma,
+                   weibull,
+                   lnorm,
+                   exp)
+  
+  legend <- c("norm",
+              "gamma",
+              "weibull",
+              "lnorm",
+              "exp")
+  
+  results <-
+    fitdistrplus::gofstat(all_dist,
+                          fitnames = legend,
+                          discrete = F)
+  
+  result <- get(names(sort(results[["aic"]])[1]))
+  
+  return(result)
+      
+}
